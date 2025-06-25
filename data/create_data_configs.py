16e732f4b2b20ae69612ff4890be1692969c27ee
JP-SystemsX <60209734+JP-SystemsX@users.noreply.github.com>
2025-06-25 09:09:11 +0200

    updated data configs

---
+++ data/create_data_configs.py
@@ -0,0 +1,60 @@
+import yaml
+import typer
+from typing import Optional
+from ConfigSpace import ConfigurationSpace, Categorical, Integer, Float
+import numbers
+
+
+def all_subsets(collection: list):
+    if len(collection) == 0:
+        return [collection]
+    else:
+        components = all_subsets(collection=collection[1:])
+        return [collection[0:1] + component for component in components] + components
+
+def build_search_space(seed: Optional[int] = None) -> ConfigurationSpace:
+    cs = ConfigurationSpace(seed=seed)
+
+    corpora = [
+        "Time_Corpus_Processed",
+        "UCI_Corpus_Processed",
+        "Lotsa_Corpus",
+        "Chronos_Corpus",
+    ]
+    corpora = all_subsets(corpora)
+    corpora = [corpus for corpus in corpora if corpus] # Remove empty list
+
+    cs.add(
+        Categorical(
+            name="corpora",
+            items=corpora,
+            default=["Chronos_Corpus"]
+        )
+    )
+    cs.add(Integer("k", (1, 6), default=3))
+    cs.add(Categorical("length", [64, 128, 256, 512, 1024, 2048], default=512))
+    cs.add(Float("alpha", (1e-3, 25.0), log=True, default=1))
+    cs.add(Float("small_ts_share", (0.05, 0.5), default=0.1))
+    cs.add(Categorical("deduplication", [1, 0], default=0))
+
+    return cs
+
+def main(
+    count: int = typer.Option(5, "--count", "-n", help="Number of configs to generate"),
+    seed: Optional[int] = typer.Option(None, "--seed", help="Random seed"),
+):
+    """
+    Sample `count` random configurations from the defined search space
+    and print each as YAML.
+    """
+    cs = build_search_space(seed)
+    configs = [dict(cs.get_default_configuration())] + [dict(config) for config in cs.sample_configuration(count-1)]
+    
+    print(configs)
+    for i, config in enumerate(configs):
+        config = {key: val if not isinstance(val, numbers.Number) else float(val) for key, val in config.items()}
+        with open(f"./data/data_configs/{i}.yml", "w") as file:
+            yaml.safe_dump(config, file)
+
+if __name__ == "__main__":
+    typer.run(main)
