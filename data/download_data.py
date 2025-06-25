c79b6bc2143a191c35a42395198dfa904f76dc20
JP-SystemsX <60209734+JP-SystemsX@users.noreply.github.com>
2025-06-25 12:55:06 +0200

    Removing Zero Shot Test Set from all corpora

--- data/download_data.py
+++ data/download_data.py
@@ -6,6 +6,10 @@ from typer import Typer, Option
 import subprocess
 from shutil import which
 import tempfile
+from typing import Set
+from normality import normalize
+from shutil import rmtree
+from functools import cache
 
 app = Typer()
 
@@ -15,50 +19,61 @@ def download_corpus(corpus_name: str = Option(..., help="Name of the corpus to d
     """
     Download the specified time series corpus.
     """
+    # load test set names
+    with open(r"..\chronos_pkg\scripts\evaluation\configs\zero-shot.yaml", "r") as f:
+        test_set = yaml.safe_load(f)
+        test_set_names = [cfg["name"] for cfg in test_set]
+
     if corpus_name == "kaggle":
-        download_kaggle_corpus()
+        download_kaggle_corpus(test_set_names=test_set_names)
     elif corpus_name == "uci":
-        download_uci_corpus()
+        download_uci_corpus(test_set_names=test_set_names)
     elif corpus_name == "chronos":
-        download_chronos_corpus()
+        download_chronos_corpus(test_set_names=test_set_names)
     elif corpus_name == "lotsa":
-        download_lotsa_corpus()
+        download_lotsa_corpus(test_set_names=test_set_names)
     elif corpus_name == "all":
         # Download all Sequentially
-        download_kaggle_corpus()
-        download_chronos_corpus()
-        download_lotsa_corpus()
+        download_kaggle_corpus(test_set_names=test_set_names)
+        download_chronos_corpus(test_set_names=test_set_names)
+        download_uci_corpus(test_set_names=test_set_names)
+        download_lotsa_corpus(test_set_names=test_set_names)
     else:
         print("Invalid corpus name. Please choose from 'kaggle', 'chronos', or 'lotsa'.")
 
 
-def download_kaggle_corpus():
+def download_kaggle_corpus(test_set_names):
     if which("kaggle") is None:
         print("kaggle missing. pip install kaggle to proceed.")
         return
     # Download Data but dont HF-Cache TS-Corpus has its own caching mechanism
     with tempfile.TemporaryDirectory(dir=cache_path) as tmp_cache_dir:
         kaggle_dataset = ds.load_dataset("ddrg/kaggle-time-series-datasets", "TIME_SERIES", trust_remote_code = True, cache_dir=tmp_cache_dir)
+        # Filter out potential matches from test set
+        kaggle_dataset = kaggle_dataset.filter(lambda x: not is_in_collection(x["name"], test_set_names))
         kaggle_dataset.save_to_disk(output_path/"Time_Corpus")
 
         
-def download_uci_corpus():
+def download_uci_corpus(test_set_names):
     # Download Data but dont HF-Cache TS-Corpus has its own caching mechanism
     with tempfile.TemporaryDirectory(dir=cache_path) as tmp_cache_dir:
         dataset = ds.load_dataset("ddrg/time-series-datasets", "TIME_SERIES", trust_remote_code = True, cache_dir=tmp_cache_dir)
+        dataset = dataset.filter(lambda x: not is_in_collection(x["name"], test_set_names))
         dataset.save_to_disk(output_path/"UCI_Corpus")
 
 
-def download_chronos_corpus():
-    subdatasets = []
+def download_chronos_corpus(test_set_names):
     for super_dataset in ["autogluon/chronos_datasets", "autogluon/chronos_datasets_extra"]:
         subdataset_names = ds.get_dataset_config_names(super_dataset, trust_remote_code=True)
         for subdataset_name in subdataset_names:
             sub_ds = ds.load_dataset(super_dataset, subdataset_name, trust_remote_code=True, cache_dir=cache_path)
-            sub_ds.save_to_disk(output_path/f"Chronos_Corpus/{subdataset_name}")
+            if subdataset_name in test_set_names:
+                sub_ds.save_to_disk(output_path/f"Chronos_Corpus_ZEROSHOT/{subdataset_name}")
+            else:
+                sub_ds.save_to_disk(output_path/f"Chronos_Corpus/{subdataset_name}")
 
 
-def download_lotsa_corpus():
+def download_lotsa_corpus(test_set_names):
     repo = "Salesforce/lotsa_data"
     local_dir = str(output_path/"Lotsa_Corpus")
 
@@ -68,6 +83,56 @@ def download_lotsa_corpus():
         "--repo-type=dataset",
         "--local-dir", local_dir
     ], check=True)
+
+    # Delete all DS in the test set from it 
+    # loop over all ds in folder
+    local_dir = Path(local_dir)
+    downloaded_datasets = [(dir.name, dir) for dir in local_dir.iterdir() if dir.is_dir() and dir.name[0] != "."]
+    for dataset_name, dataset_folder in downloaded_datasets:
+        if is_in_collection(dataset_name, test_set_names):
+            # deleted dataset again to not accidentally train on test set
+            rmtree(dataset_folder)
+
+    
+
+# Utils #
+
+
+def char_bigrams(s: str) -> Set[str]:
+    """Return the set of character bigrams for string s."""
+    return {s[i : i + 2] for i in range(len(s) - 1)} if len(s) > 1 else set()
+
+def bigram_similarity(s1: str, s2: str) -> float:
+    """
+    Compute Jaccard similarity between two strings based on character bigrams.
+    Returns 1.0 if both strings yield no bigrams.
+    """
+    b1, b2 = char_bigrams(f" {s1} "), char_bigrams(f" {s2} ")
+    if not b1 and not b2:
+        return 1.0
+    inter = b1 & b2
+    union = len(b1) + len(b2)
+    return len(inter) * 2 / union
+
+@cache
+def my_normalize(text: str|list[str]):
+    if isinstance(text, str):
+        return normalize(text, latinize=True, ascii=True)
+    elif isinstance(text, list):
+        return [my_normalize(word) for word in text]
+    else:
+        raise Exception(f"Text has unsupported dtype {type(text)}")
+
+def is_in_collection(orig: str, collection: list[str]):
+    orig = my_normalize(orig)
+    collection = my_normalize(tuple(collection)) # to tuple for caching
+    for word in collection:
+        score = bigram_similarity(orig, word)
+        if score >= 0.8:
+            print(f"{orig} matches {word}!")
+            return True
+    return False
+        
 
 
 
