bbbbfc6d9f24fad62fc9239f8d62d8d3d55379c0
JuliusHanuschh <julius.hanusch@mailbox.tu-dresden.de>
2026-06-18 10:51:25 +0200

    Anomaly Hack

--- src/finetuning/evaluate_anomaly.py
+++ src/finetuning/evaluate_anomaly.py
@@ -142,7 +142,7 @@ def evaluate_dataset_no_windowing(
     dataset_name,
     tokenizer,
     max_length: int = 512,
-    threshold: float = 0.9,
+    threshold: float = 0.95,
     device: str = "cuda" if torch.cuda.is_available() else "cpu",
 ):
 
