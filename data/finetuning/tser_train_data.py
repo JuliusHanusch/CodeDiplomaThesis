2e195d5bcf7ca6c6387630ee82f095e45735749a
JuliusHanuschh <julius.hanusch@mailbox.tu-dresden.de>
2026-06-22 11:54:09 +0200

    TSER

--- data/finetuning/tser_train_data.py
+++ data/finetuning/tser_train_data.py
@@ -30,7 +30,7 @@ def convert_example(ex, idx):
         "start": ex["start"],
         "target": ts,
         "item_id": str(ex.get("item_id", idx)),
-        "feat_static_cat": feat_static_cat,  # ✅ FIXED
+        #"feat_static_cat": feat_static_cat,  # ✅ FIXED
         "to_predict": float(ex["to_predict"]),
     }
 
