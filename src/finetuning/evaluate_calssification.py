e43fd1774b392b3e74b2a4f1ba60b30bc211a14c
JuliusHanuschh <julius.hanusch@mailbox.tu-dresden.de>
2026-06-23 20:21:54 +0200

    Classification

--- src/finetuning/evaluate_calssification.py
+++ src/finetuning/evaluate_calssification.py
@@ -101,11 +101,13 @@ def evaluate_model(model, tokenizer, X, y, batch_size=32):
 # -----------------------------
 if __name__ == "__main__":
 
-    model_path = "/content/CodeDiplomaThesis/FineTunedModels/classification/run-10/checkpoint-final/"
+    model_path = "/content/CodeDiplomaThesis/FineTunedModels/classification/run-12/checkpoint-final/"
 
     #tsv_path = "/content/CodeDiplomaThesis/data/finetuning/UCR_extracted/UCRArchive_2018/DistalPhalanxTW/DistalPhalanxTW_TEST.tsv"
     #tsv_path = "/content/CodeDiplomaThesis/data/finetuning/UCR_extracted/UCRArchive_2018/ArrowHead/ArrowHead_TEST.tsv"
-    tsv_path = "/content/CodeDiplomaThesis/data/finetuning/UCR_extracted/UCRArchive_2018/GestureMidAirD2/GestureMidAirD2_TEST.tsv"
+    #tsv_path = "/content/CodeDiplomaThesis/data/finetuning/UCR_extracted/UCRArchive_2018/GestureMidAirD2/GestureMidAirD2_TEST.tsv"
+    tsv_path = "/content/CodeDiplomaThesis/data/finetuning/UCR_extracted/UCRArchive_2018/Wafer/Wafer_TEST.tsv"
+
 
 
     num_labels = 26
