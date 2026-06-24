bca7a929c1ddf3aa2a53d115442a73028b51538d
JP-SystemsX <60209734+JP-SystemsX@users.noreply.github.com>
2026-06-24 11:03:19 +0200

    Merge conflicts

--- data/train/get_train_data.py
+++ data/train/get_train_data.py
@@ -8,7 +8,7 @@ from tqdm import tqdm  # Import tqdm for progress bar
 print("Start Converting Data")
 
 #Mix = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_tsmixup_10m", streaming=True, split="train") 
-Kernel = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_kernel_synth_1m", streaming=True, split="train").take(100)
+Kernel = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_kernel_synth_1m", streaming=True, split="train").take(10000)
 
 
 
@@ -30,6 +30,6 @@ def convert_to_arrow(
     ArrowWriter(compression=compression).write_to_file(dataset, path=path)
 
 #convert_to_arrow("./training_mix.arrow", time_series=Mix_time_series)
-convert_to_arrow("./kernelsynth.arrow", time_series=Kernel_time_series)
+convert_to_arrow("./data/train/kernelsynth.arrow", time_series=Kernel_time_series)
 
 
