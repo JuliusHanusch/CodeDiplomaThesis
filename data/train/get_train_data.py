97943b8d14edf16b1145c756300012fb366c48fe
JuliusHanuschh <julius.hanusch@mailbox.tu-dresden.de>
2026-06-24 09:26:22 +0200

    train data script

---
+++ data/train/get_train_data.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from typing import List, Union
+import numpy as np
+import datasets
+from gluonts.dataset.arrow import ArrowWriter
+from tqdm import tqdm  # Import tqdm for progress bar
+
+print("Start Converting Data")
+
+Mix = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_tsmixup_10m", streaming=True, split="train") # oder "training_corpus_tsmixup_10m"
+Kernel = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_kernel_synth_1m", streaming=True, split="train") # oder "training_corpus_tsmixup_10m"
+
+
+
+Mix_time_series = [np.array(data['target']) for data in tqdm(Mix, desc="Processing time series")]
+Kernel_time_series = [np.array(data['target']) for data in tqdm(Kernel, desc="Processing time series")]
+
+
+def convert_to_arrow(
+    path: Union[str, Path],
+    time_series: Union[List[np.ndarray], np.ndarray],
+    compression: str = "lz4",
+):
+    assert isinstance(time_series, list) or (
+        isinstance(time_series, np.ndarray) and
+        time_series.ndim == 2
+    )
+    start = np.datetime64("2000-01-01 00:00", "s")
+    dataset = [{"start": start, "target": ts} for ts in tqdm(time_series, desc="Converting to Arrow")]
+    ArrowWriter(compression=compression).write_to_file(dataset, path=path)
+
+convert_to_arrow("./kernelsynth.arrow", time_series=Mix_time_series)
+convert_to_arrow("./training_mix.arrow", time_series=Kernel_time_series)
+
+
+print("Dataset successfully converted and stored in 'kernelsynth.arrow'")
