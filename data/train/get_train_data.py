3d5075d58a5c28a3d329abdd28f97af5b120e043
JuliusHanusch <julius.hanusch@mailbox.tu-dresden.de>
2026-06-25 09:45:33 +0200

    Mix Data

--- data/train/get_train_data.py
+++ data/train/get_train_data.py
@@ -7,13 +7,13 @@ from tqdm import tqdm  # Import tqdm for progress bar
 
 print("Start Converting Data")
 
-#Mix = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_tsmixup_10m", streaming=True, split="train") 
-Kernel = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_kernel_synth_1m", streaming=True, split="train").take(10000)
+Mix = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_tsmixup_10m", streaming=True, split="train").take(1000)
+#Kernel = datasets.load_dataset("autogluon/chronos_datasets", "training_corpus_kernel_synth_1m", streaming=True, split="train").take(10000)
 
 
 
-#Mix_time_series = [np.array(data['target']) for data in tqdm(Mix, desc="Processing time series")]
-Kernel_time_series = [np.array(data['target']) for data in tqdm(Kernel, desc="Processing time series")]
+Mix_time_series = [np.array(data['target']) for data in tqdm(Mix, desc="Processing time series")]
+#Kernel_time_series = [np.array(data['target']) for data in tqdm(Kernel, desc="Processing time series")]
 
 
 def convert_to_arrow(
@@ -29,7 +29,7 @@ def convert_to_arrow(
     dataset = [{"start": start, "target": ts} for ts in tqdm(time_series, desc="Converting to Arrow")]
     ArrowWriter(compression=compression).write_to_file(dataset, path=path)
 
-#convert_to_arrow("./training_mix.arrow", time_series=Mix_time_series)
-convert_to_arrow("./data/train/kernelsynth.arrow", time_series=Kernel_time_series)
+convert_to_arrow("./training_mix.arrow", time_series=Mix_time_series)
+#convert_to_arrow("./data/train/kernelsynth.arrow", time_series=Kernel_time_series)
 
 
