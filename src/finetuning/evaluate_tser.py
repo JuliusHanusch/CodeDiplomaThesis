45244ce32d1b30b8f2bf805baed9c3b512683cb8
JuliusHanuschh <julius.hanusch@mailbox.tu-dresden.de>
2026-06-29 10:54:28 +0200

    TSER

--- src/finetuning/evaluate_tser.py
+++ src/finetuning/evaluate_tser.py
@@ -212,7 +212,7 @@ def evaluate_all_baselines(ridge_model, test_arrow_path, context_length=512):
 # -------------------------
 if __name__ == "__main__":
 
-    model_path = "/content/CodeDiplomaThesis/FineTunedModels/TSER/run-1/checkpoint-final"
+    model_path = "/content/CodeDiplomaThesis/FineTunedModels/TSER/run-2/checkpoint-final"
     test_dataset = "/content/CodeDiplomaThesis/data/finetuning/TSER/PPGDalia/test.arrow"
     train_dataset = "/content/CodeDiplomaThesis/data/finetuning/TSER/PPGDalia/train.arrow"
 
